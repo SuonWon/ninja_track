e8838b13fd815fed98d69f084d06502e1a62e1a5
Aung Han Soe <aunghansoe.mdy@gmail.com>
2023-07-26 11:34:38 +0630

    fet: testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,112 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  
+  const [country, setCountry] = useState([]);
+  const [selectCountry, setSelecteCountry] = useState(null);
+  const [therapist, setTherapist] = useState([]);
+  const [medium, setMedium] = useState([]);
+  const [service, setService] = useState([]);
+
+  const dummy = ['64af8a3c1f401705c1b4c9fd', '64af8a3c1f401705c1b4c9fc','64af8a3c1f401705c1b4c9fb']
+  // getData();
+
+  // getTherapist('64af8a3c1f401705c1b4c9fd');
+
+  useEffect(() => {
+    getData();
+    if(selectCountry) {
+      getTherapist(selectCountry);
+    }
+    
+    console.log(selectCountry);
+  }, [selectCountry])
+
+  function getData() {
+    getCountry();
+    getMeidum();
+    getService();
+  }
+
+  async function getCountry() {
+    const country = await fetch('http://localhost:3000/graphql', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+          query: `
+          query ExampleQuery {
+            getAllCountries {
+              id
+              country
+              code
+              currency
+              currency_symbol
+              flag
+              status
+              wallet_policy
+              withdraw_fee
+            }
+          }
+          `,
+      })
+    })
+
+    const data = await country.json();
+
+    setCountry(data.data.getAllCountries);
+  }
+
+  async function getMeidum() {
+    
+  }
+
+  async function getService() {
+
+  }
+
+  async function getTherapist(countryId) {
+    console.log(countryId);
+    const response = await fetch('http://localhost:3000/graphql', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+          query: `
+          query ExampleQuery($countryId: String!) {
+            getTherapistsByCountry(countryId: $countryId) {
+              id
+              first_name
+              middle_name
+              last_name
+              location
+              country_id
+            }
+          }          
+          `, 
+          variables: {
+              "countryId": countryId
+            }
+      })
+    })
+
+    const data = await response.json();
+
+    setTherapist(data.data.getTherapistsByCountry);
+  }
+
+  // handleClick = () => {
+  //   console.log('clicked');
+  // }
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +122,17 @@ function App() {
         >
           Learn React
         </a>
+        <select value={selectCountry} onChange={handleChange}>
+          {
+            country.length > 0 ? (
+              country.map(({id, country}) => {
+                return(<option value={id}>{country}</option>)
+              })
+            ) : null
+            
+          }
+          
+        </select>
       </header>
     </div>
   );
